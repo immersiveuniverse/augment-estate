6a02ee4b347a8ef6d2677c6438728effa6e06c48
Amandeep Singh <adsoapps@gmail.com>
2019-09-05 09:30:04 +0530

    Publishing Augment Estate Project
    uploading augment estate project on github

---
+++ Assets/Resources/Scripts/ButtonController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	public GameObject hInt, hOut, panelHelp, tv;
+
+	void Start(){
+		hInt.SetActive (false);
+		hOut.SetActive (false);
+		tv.SetActive (false);	
+	}
+
+	public void SetInteriorActive(){
+		hInt.SetActive (true);
+		hOut.SetActive (false);
+		tv.SetActive (false);
+	}
+
+	public void SetOutsideActive(){
+		hInt.SetActive (false);
+		tv.SetActive (false);
+		hOut.SetActive (true);
+	}
+
+	public void OnClickBtnHelp(){
+
+		if (panelHelp.activeSelf==true) {
+			panelHelp.SetActive (false);
+		}else if (panelHelp.activeSelf==false) {
+			panelHelp.SetActive (true);
+		}
+	}
+
+	public void OnCLickBtnInfo(){
+		if (tv.activeSelf==true) {
+			tv.SetActive (false);
+		}else if (tv.activeSelf==false) {
+			hOut.SetActive (false);
+			hInt.SetActive (false);
+			tv.SetActive (true);
+
+		}
+	}
+}
